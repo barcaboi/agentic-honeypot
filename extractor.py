5ee404f95336e48e9f30b873b64ea8c78f1d0085
Suraj17 <ronakk.17@outlook.com>
2026-02-04 22:07:33 +0530

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -1,1 +1,13 @@
+import re
+
+BANK_REGEX = r"\b\d{9,18}\b"
+UPI_REGEX = r"\b[\w.-]+@[\w]+\b"
+URL_REGEX = r"https?://[^\s]+"
+
+def extract_intel(text: str):
+    return {
+        "bank_accounts": re.findall(BANK_REGEX, text),
+        "upi_ids": re.findall(UPI_REGEX, text),
+        "phishing_urls": re.findall(URL_REGEX, text)
+    }
 
